ce2de6fec9ad9aa0493a01f648b311610b97fd11
sainath <sainathbakawade9@gmail.com>
2024-07-07 21:20:34 +0530

    authentication

--- backend/middleware/isAuthenticated.js
+++ backend/middleware/isAuthenticated.js
@@ -1,14 +1,14 @@
 import jwt from "jsonwebtoken";
-const isAuthenticated = async(req,res,next) => {
+const isAuthenticated = async (req, res, next) => {
   try {
     const token = req.cookies.token;
-    if(!token){
-        return res.status(401).json({message:"User not authenticated."})
-    };
-    const decode = await jwt.verify(token,process.env.JWT_SECRET_KEY);
-    if(!decode){
-        return res.status(401).json({message:"Invalid token"});
-    };
+    if (!token) {
+      return res.status(401).json({ message: "User not authenticated." });
+    }
+    const decode = await jwt.verify(token, process.env.JWT_SECRET_KEY);
+    if (!decode) {
+      return res.status(401).json({ message: "Invalid token" });
+    }
     req.id = decode.userId;
     next();
   } catch (error) {
@@ -18,6 +18,6 @@ const isAuthenticated = async(req,res,next) => {
 export default isAuthenticated;
 
 const req = {
-    id:"",
-}
-req.id = "sdlbgnjdfn"
+  id: "",
+};
+req.id = "sdlbgnjdfn";
